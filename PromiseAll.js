e6a29500a60f0901f254d2db8d74115a88576ffd
King1376 <1185079648@qq.com>
2021-11-14 21:15:49 +0800

    feat: 优化Promise.all实现

--- PromiseAll.js
+++ PromiseAll.js
@@ -1,28 +1,31 @@
 
 /**
  * 
- * @param {迭代器} iterable 
+ * @param {迭代器, 数组、Set、Map} iterable 
  * @returns 返回一个Promise对象
  */
 const promiseAll = iterable => {
     // 1. 返回Promise对象
     return new Promise((resolve, reject) => {
         try {
-            // 2. 迭代器转换成数组
-            const list = Array.from(iterable)
-            const result = [],
-                  count = 0;
-           // 3. 遍历数组，按顺序插入新的数组
-            for(let i = 0; i < list.length; i++) {
-                Promise.resolve(list[i]).then(res => {
-                    result[i] = res
+            // 2. 同义转换成数组
+            const list = [...iterable]
+            const result = []
+            let count = 0
+            if(!list.length) {
+                resolve(result)
+            }
+           // 3. 需要原属组的顺序返回，所以根据它的索引来插入数据
+            list.forEach((item, idx) => {
+                Promise.resolve(item)
+                .then(res => {
+                    result[idx] = res
                     if(list.length === ++count) {
                         resolve(result)
                     }
-                }).catch(error => reject(error))
-
-            }
-            result.length === list.length && resolve(result)
+                })
+                .catch(error => reject(error))
+            })
         } catch (error) {
             reject(error)
         }
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Promise
-实现Promise
+# Promise.all
+- 1. 参数必须是可迭代对象，如 Array 、Map 、Set
+- 2. 返回一个Promise对象，值为一数组
+- 3. 若参数的每一项不是一个Promise实例，则通过Promise.resolve来新建一个状态为
+    fulfilled的Promise实例
+- 4. 只要有一个Promise实例失败，则它的结果也是失败的
+- 5. 返回的Promise实例数组不是按照Promise实例完成的顺序，而是参数的顺序返回的
