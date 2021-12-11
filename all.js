9d842dae7b6efe5f73ba48cac1bb8e9dd4d02621
King1376 <1185079648@qq.com>
2021-12-11 23:37:12 +0800

    feat: 实现Promise.all

--- all.js
+++ all.js
@@ -1,19 +1,14 @@
-function kAll(iterable) {
-    // 返回一个Promise对象
+function promiseAall(iterable) {
     return new Promise((resolve, reject) => {
         try {
-            // 先把迭代器转换成数组
-            const list = [...iterable]
-            const result = []
+            const list = [ ...iterable ],
+                  result = [];
             let count = 0
-            if(!list.length) {
-                resolve(result)
+            if(!list.length){
+                return result
             }
             list.forEach((item, idx) => {
-                // 为了保证数组的每一项都是Promise对象，先把Promise.resolve()转换成
-                // Promise对象，在进行遍历
                 Promise.resolve(item).then(res => {
-                    // 为了保证原来顺序，通过索引来插值
                     result[idx] = res
                     if(list.length === ++count) {
                         resolve(result)
@@ -23,8 +18,6 @@ function kAll(iterable) {
         } catch (error) {
             reject(error)
         }
-        
     })
 }
-// 挂在Promise上
-Promise.kAll = kAll
+Promise.promiseAll = promiseAall
